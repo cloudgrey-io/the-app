a81519a92ca0ab3cabc2031ab9edf1065ebbb14d
Jonathan Lipps <jlipps@gmail.com>
2018-02-19 22:36:20 -0800

    boilerplate and react native navigation

--- App.js
+++ App.js
@@ -9,50 +9,43 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
+import {
+  Header,
+  List,
+  ListItem,
+} from 'react-native-elements';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const viewList = [
+  {name: "Echo Box", desc: "Write something and save to local memory"},
+];
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+      <View>
+        <Header backgroundColor="#000" centerComponent={{text: "The App", style: styles.header}}/>
+        <Text style={styles.listHeader}>Choose An Awesome View</Text>
+        <List>
+          {viewList.map((l, i) => (
+            <ListItem key={i} title={l.name} subtitle={l.desc} />
+          ))}
+        </List>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
+  header: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    color: '#fff'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  listHeader: {
+    padding: 8,
+    fontSize: 16,
+    marginBottom: -20,
   },
 });
--- android/app/src/main/java/com/theapp/MainActivity.java
+++ android/app/src/main/java/com/theapp/MainActivity.java
@@ -1,15 +1,7 @@
 package com.theapp;
 
-import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.controllers.SplashActivity;
 
-public class MainActivity extends ReactActivity {
-
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "TheApp";
-    }
+public class MainActivity extends SplashActivity {
+    
 }
