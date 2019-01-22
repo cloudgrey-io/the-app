d589927ad90a5bf8dc3487297376a5e38ee0e373
Jonah Stiennon <jonahss@gmail.com>
2019-01-22 15:44:44 -0800

    android updates

--- android/app/src/main/java/io/cloudgrey/the_app/MainApplication.java
+++ android/app/src/main/java/io/cloudgrey/the_app/MainApplication.java
@@ -5,7 +5,10 @@ import android.widget.Toast;
 
 import com.centaurwarchief.smslistener.SmsListenerPackage;
 import com.facebook.react.ReactPackage;
+
 import com.reactnativenavigation.NavigationApplication;
+import com.reactnativenavigation.react.NavigationReactNativeHost;
+import com.reactnativenavigation.react.ReactGateway;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,6 +19,17 @@ public class MainApplication extends NavigationApplication {
     public void onCreate() {
         super.onCreate();
         WebView.setWebContentsDebuggingEnabled(true);
+    }
+
+    @Override
+    protected ReactGateway createReactGateway() {
+        ReactNativeHost host = new NavigationReactNativeHost(this, isDebug(), createAdditionalReactPackages()) {
+            @Override
+            protected String getJSMainModuleName() {
+                return "index";
+            }
+        };
+        return new ReactGateway(this, isDebug(), host);
     }
 
     @Override
